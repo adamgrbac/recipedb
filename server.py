7e24dea9d137ebbbfa8ec5e840f5c517758605e1
Adam Grbac <adam.grbac@gmail.com>
2024-01-03 09:47:40 +1000

    Initial Commit

---
+++ server.py
@@ -0,0 +1,103 @@
+from flask import Flask, render_template, request
+import requests
+import sqlite3
+import os
+
+
+# SQLITE DB
+con = sqlite3.connect("recipes.db", check_same_thread=False)
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+	return render_template("index.html")
+    
+@app.route("/recipes")
+def all_recipes():
+    cur = con.cursor()
+    cur.execute(f"""
+    SELECT 
+      * 
+    FROM 
+      recipes
+    ORDER BY name
+    """)
+    recipes = cur.fetchall()
+    return render_template("recipes.html", recipes=recipes)
+    
+@app.route("/recipes/<int:id>")
+def recipe(id):
+    cur = con.cursor()
+    cur.execute(f"""
+    SELECT 
+      * 
+    FROM 
+      recipes 
+    WHERE
+      id = {id}
+    """)
+    recipe = cur.fetchone()
+    
+    cur.execute(f"""
+    SELECT 
+      ingredients.*,
+      recipe_ingredient.quantity,
+      recipe_ingredient.unit,
+      recipe_ingredient.comment      
+    FROM 
+      recipe_ingredient 
+    INNER JOIN
+      ingredients on recipe_ingredient.ingredient_id = ingredients.id
+    WHERE
+      recipe_ingredient.recipe_id = {id}
+    """)
+    ingredients = cur.fetchall()
+    return render_template("recipe.html", recipe=recipe, ingredients=ingredients)
+    
+@app.route("/search")
+def search():
+    query = request.args.get("q")
+    cur = con.cursor()
+    cur.execute(f"""
+    SELECT 
+      * 
+    FROM 
+      recipes 
+    WHERE 
+      lower(name) like '%{query.lower()}%'
+    
+    UNION
+    
+    SELECT DISTINCT 
+      recipes.* 
+    FROM
+      recipes
+    INNER JOIN
+      recipe_ingredient ON recipes.id = recipe_ingredient.recipe_id
+    INNER JOIN
+      ingredients ON recipe_ingredient.ingredient_id = ingredients.id
+    WHERE
+      lower(ingredients.name) like '%{query.lower()}%' 
+    ;
+    """)
+    res = cur.fetchall()
+    return res
+
+@app.route("/get_token")
+def callback():
+    code = request.args.get("code")
+    res = requests.post("https://todoist.com/oauth/access_token",
+                        data={
+                            "client_id": os.getenv("todoist_client_id"),
+                            "client_secret": os.getenv("todoist_client_secret"),
+                            "code": code,
+                            "redirect_uri":"http://localhost:5000/token"
+                        })
+    return res.json()["access_token"]
+
+@app.route("/add_item")
+def add_item():
+    pass
+ 
+if __name__ == "__main__":
+	app.run()
---
+++ src/create_db.py
@@ -0,0 +1,142 @@
+import os.path
+import sqlite3
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+# The ID and range of a sample spreadsheet.
+SPREADSHEET_ID = "1LFY_aU3TxbCH_pXZeHcaRKFopdZhGXSk5Sm89xUXk4U"
+
+# SQLITE DB
+con = sqlite3.connect("../recipes.db")
+
+cur = con.cursor()
+
+cur.execute("""
+    DROP TABLE IF EXISTS recipes;
+""")
+cur.execute("""
+    DROP TABLE IF EXISTS ingredients;
+""")
+cur.execute("""
+    DROP TABLE IF EXISTS recipe_ingredient;
+""")
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS recipes (
+        id INTEGER PRIMARY KEY,
+        name char(50),
+        description char(50),
+        book char(50),
+        page_number char(50),
+        link char(50)
+    );
+""")
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS ingredients (
+        id INTEGER PRIMARY KEY,
+        name char(50),
+        category char(50)
+    );
+""")
+cur.execute("""
+    CREATE TABLE IF NOT EXISTS recipe_ingredient (
+        recipe_id INTEGER,
+        ingredient_id INTEGER,
+        quantity char(50),
+        unit char(50),
+        comment char(256)
+    );
+""")
+
+def parse_sheet(values):
+    # Parse Recipe details
+    recipe = values[2:7]
+    recipe_metadata = {x[1]: (x[2] if len(x) > 2 else "") for x in recipe}
+
+    # Parse Ingredients
+    ingredients = values[9:]
+    headers = ingredients[0][1:]
+    ingredients_metadata = [dict(zip(headers,x[1:])) for x in ingredients[1:]]
+
+    return recipe_metadata, ingredients_metadata
+
+
+def main():
+  """Shows basic usage of the Sheets API.
+  Prints values from a sample spreadsheet.
+  """
+  creds = None
+  # The file token.json stores the user's access and refresh tokens, and is
+  # created automatically when the authorization flow completes for the first
+  # time.
+  if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+  # If there are no (valid) credentials available, let the user log in.
+  if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+      creds.refresh(Request())
+    else:
+      flow = InstalledAppFlow.from_client_secrets_file(
+          "credentials.json", SCOPES
+      )
+      creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open("token.json", "w") as token:
+      token.write(creds.to_json())
+
+  try:
+    service = build("sheets", "v4", credentials=creds)
+
+    # Call the Sheets API
+    sheet = service.spreadsheets().get(spreadsheetId=SPREADSHEET_ID).execute()
+    for tab in sheet["sheets"]:
+        tab_name = tab["properties"]["title"]
+        tab_range = f"{tab_name}!A1:F"
+        sheet = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID, range=tab_range).execute()
+        if sheet["range"].split("!")[0] not in ("Instructions","TEMPLATE", "LOOKUPS"):
+            recipe, ingredients = parse_sheet(sheet["values"])
+            cur.execute("""
+                INSERT INTO recipes (name, description, book, page_number, link) VALUES (?, ?, ?, ?, ?)
+            """, [x.lower() for x in recipe.values()])
+            con.commit()
+            res = cur.execute("""
+                SELECT id FROM recipes where name = ?
+            """, [recipe["Name"].lower()])
+            recipe_id = res.fetchone()[0]
+            
+            # Insert unique ingredients
+            for ingredient in ingredients:
+                if ingredient["Name"].strip() != "":
+                    res = cur.execute("""
+                        SELECT id FROM ingredients where name = ?
+                    """, [ingredient["Name"].strip().lower()])
+                    if len(res.fetchall()) == 0:
+                        cur.execute("""
+                            INSERT INTO ingredients (name, category) VALUES (?, ?) 
+                        """, [ingredient["Name"].strip().lower(), ingredient["Category"].strip().lower()])
+                        con.commit()
+            
+            for ingredient in ingredients:
+                print(ingredient)
+                if ingredient["Name"].strip() != "":
+                    res = cur.execute("""
+                        SELECT id FROM ingredients where name = ?
+                    """, [ingredient["Name"].strip().lower()])
+                    cur.execute("""
+                        INSERT INTO recipe_ingredient VALUES (?, ?, ?, ?, ?)
+                    """,[recipe_id, res.fetchone()[0], ingredient.get("Quantity",""), ingredient.get("Unit",""), ingredient.get("Comment","")])
+                    con.commit()
+            
+  except HttpError as err:
+    print(err)
+
+
+if __name__ == "__main__":
+  main()
